ab481450b0d29e2a5ae149a9cdebf3c25a308f54
EyuelMathewos <eyuelmathewos@gmail.com>
2022-07-08 10:11:52 +0300

    update user steps

--- features/step_definitions/user_steps.js
+++ features/step_definitions/user_steps.js
@@ -2,22 +2,24 @@ const { Given, When, Then } = require('@cucumber/cucumber');
 const Validator = require('validatorjs');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient()
-let data,response, error;
-let statusValues=[];
+let data;
+let statusValues = {};
 const createValidation = {
   "email": "required|email",
   "firstName": "required|string",
   "lastName": "required|string"
 };
-const validator = ( body, rules, customMessages ) => {
-    return new Promise(async function (resolve, reject) {
-        const validation = new Validator(body, rules, customMessages);
-        validation.passes(() => resolve({status:true}));
-        validation.fails(() => {
-            validation.errors.status=false
-            reject(validation.errors)
-        });
-    })
+const validator = (body, rules, customMessages) => {
+  return new Promise(async function (resolve, reject) {
+    const validation = new Validator(body, rules, customMessages);
+    validation.passes(() => resolve({
+      status: true
+    }));
+    validation.fails(() => {
+      validation.errors.status = false
+      reject(validation.errors)
+    });
+  })
 };
 Given('account information', function (dataTable) {
   data = dataTable.hashes()
@@ -25,29 +27,30 @@ Given('account information', function (dataTable) {
 });
 
 When('registers for a new account', async function () {
- // data
   for (const key in data) {
-    //console.log(`value `,data[key])
     let value = validator(data[key], createValidation, {}).then(async (response) => {
       validity = response
-      statusValues.push("pass");
-     }).catch(error=>{
-       errorval = error
-       statusValues.push("fail");
-     })
+      const valdationStatus = response.status;
+      if (valdationStatus) {
+        statusValues[key] = "pass";
+      } else {
+        statusValues[key] = "fail";
+      }
+
+    }).catch(error => {
+      console.log(error)
+      errorval = error
+      statusValues[key] = "fail";
+    })
 
   }
 });
 
 Then('the following are expected value should be', function (dataTable) {
   let compareVal = dataTable.hashes();
-    for (const key in compareVal) {
-      console.log(`${compareVal[key].email} ${compareVal[key].expected} values ${statusValues[key]} `) 
-      if(compareVal[key].expected == statusValues[key]){
-        console.log(`${compareVal[key].email} ${compareVal[key].expected} values ${statusValues[key]} `) 
-      }
-      //  else{
-      //   throw("condition not meet")
-      // }
+  for (const key in compareVal) {
+    if (compareVal[key].expected == statusValues[key]) {
+      console.log(`${compareVal[key].email} ${compareVal[key].expected} values ${statusValues[key]} `)
     }
+  }
 });
